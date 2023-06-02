bc685adec3d8e1d27a1a1fb43bacb1fe5bbbd9e9
MariaBetancur <mabo6241@gmail.com>
2023-06-02 17:07:08 -0500

    v1.4

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 //peticion HTTP
-const breedselect = document.querySelector('#breed-select');
+const breedSelect = document.querySelector('#breed-select');
+const loader = document.querySelector('.loader');
+const errorElement = document.querySelector('.error');
+
 fetch('https://api.thecatapi.com/v1/breeds')
   .then(response => {
+    loader.style.display = 'block'; // Mostrar el cargador
     if (!response.ok) {
       throw new Error('Error en la solicitud');
     }
@@ -15,56 +19,8 @@ fetch('https://api.thecatapi.com/v1/breeds')
       breedSelect.appendChild(option);
     });
     console.log(data);
+    loader.style.display = 'none';
   })
   .catch(error => {
     console.error('Error:', error);
   });
-//coleccion de razas
-import { fetchBreeds } from './cat-api.js';
-
-fetchBreeds()
-  .then(breeds => {
-    console.log(breeds);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
-//info gato
-const catinfo = document.querySelector('#cat-info');
-breedSelect.addEventListener('change', () => {
-  const breedId = breedSelect.value;
-  fetchCatByBreed(breedId)
-    .then(data => {
-      // Mostrar información del gato en catInfo
-      catInfo.innerHTML = `
-        <p>Nombre de la raza: ${data.name}</p>
-        <p>Descripción: ${data.description}</p>
-        <p>Temperamento: ${data.temperament}</p>
-        <img src="${data.image.url}" alt="Imagen del gato">
-      `;
-      console.log(data);
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
-});
-// cat-api.js
-export function fetchBreeds() {
-  return fetch('https://api.thecatapi.com/v1/breeds').then(response => {
-    if (!response.ok) {
-      throw new Error('Error en la solicitud');
-    }
-    return response.json();
-  });
-}
-
-export function fetchCatByBreed(breedId) {
-  return fetch(`https://api.thecatapi.com/v1/images/search?breed_id=${breedId}`)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Error en la solicitud');
-      }
-      return response.json();
-    })
-    .then(data => data[0].breeds[0]); // Devolver solo los datos de la raza
-}
